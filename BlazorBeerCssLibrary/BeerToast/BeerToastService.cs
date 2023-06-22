db8d54db290572bf67e5ec187a22a07a0485fa1f
KristianFriis <friis1212@gmail.com>
2023-06-22 09:05:01 +0200

    Added WIP Beer Toast Service

---
+++ BlazorBeerCssLibrary/BeerToast/BeerToastService.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorBeerCssLibrary.BeerToast;
+
+public class BeerToastService
+{
+    public ToastType ToastType {get; private set;}
+    public string ToastMessage { get; private set; } = "";
+    public string Icon { get; private set; } = "";
+    public void ShowToast(ToastType toastType, string toastMessage, string icon = "")
+    {
+        ToastType = toastType;
+        ToastMessage = toastMessage;
+        Icon = icon;
+        NotifyStateChanged();
+    }
+
+    public event Action OnChange;
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
+
+public enum ToastType
+{
+    Primary,
+    Error,
+    Success,
+}
