56d5a58c4c8d99db588a50c452fddcdd3ccde4da
KristianFriis <friis1212@gmail.com>
2023-06-28 13:35:03 +0200

    added BeerTooltip
    added ClassHelpers.CreateClass to help building of classes.

--- BlazorBeerCss/Extensions/EnumsExtensions.cs
+++ BlazorBeerCss/Extensions/EnumsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Reflection;
 
 namespace BlazorBeerCss.Extensions;
 
@@ -28,5 +29,20 @@ public static class EnumsExtensions
             return attribute.Description;
         }
         throw new ArgumentException("Item not found.", nameof(enumValue));
+    }
+
+    public static T GetEnumValueFromDescription<T>(string description)
+    {
+        MemberInfo[] fis = typeof(T).GetFields();
+
+        foreach (var fi in fis)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0 && attributes[0].Description == description)
+                return (T)Enum.Parse(typeof(T), fi.Name);
+        }
+
+        throw new Exception("Not found");
     }
 }
---
+++ BlazorBeerCss/Helpers/ClassHelpers.cs
@@ -0,0 +1,20 @@
+﻿using BlazorBeerCss.Extensions;
+using System.Text;
+
+namespace BlazorBeerCss;
+
+public static class ClassHelpers
+{
+    public static string CreateClass(string baseClass, params Enum[] ToBeAdded)
+    {
+        StringBuilder sb = new StringBuilder(baseClass, 50);
+        
+        foreach (var item in ToBeAdded)
+        {
+            if (item.GetEnumDescription() != "")
+                sb.Append($" {item.GetEnumDescription()}");
+        }
+
+        return sb.ToString();
+    }
+}
