c9af22efc0725f566116a1754ef8681609e8314e
Terkel Brix <terkel@northern-nerds.dk>
2023-06-30 09:05:49 +0200

    Implement beerchip

---
+++ BlazorBeerCss/Helpers/BeerElevations.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace BlazorBeerCss;
+
+public enum BeerElevations
+{
+    [Description("")]
+    Default,
+    [Description("large-elevate")]
+    Large,
+    [Description("medium-elevate")]
+    Medium,
+    [Description("small-elevate")]
+    Small,
+    [Description("")]
+    None,
+}
